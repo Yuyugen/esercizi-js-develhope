499ee7e2bb58a71566d5ecf4b60b18b110424002
Francesco Paul Figueroa <paul.figueroa14@gmail.com>
2023-03-12 23:48:58 +0100

    callbacks exercise 2 done

--- Exercise-35/exercise.js
+++ Exercise-35/exercise.js
@@ -1,1 +1,7 @@
+function repeatHello(callback) {
+  setInterval(callback, 1000);
+}
 
+repeatHello(() => console.log("hello"));
+
+//la callback deve essere una funzione arrow, poiché setInterval prende una funzione come argomento
